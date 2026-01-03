2bf559d06b1a39ee2b28d33e7ff06732a146b9ac
bibekkoirala1 <bibek123koirala@gmail.com>
2026-01-03 10:41:12 +0545

    worked on register

--- lib/features/onboarding/presentation/view_model/onboarding_cubit.dart
+++ lib/features/onboarding/presentation/view_model/onboarding_cubit.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hotel_booking/features/auth/presentation/view/login_view.dart';
-import 'package:hotel_booking/features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:my_app/features/auth/presentation/view_model/login/login_bloc.dart';
 
 
 
